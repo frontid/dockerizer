a56a42debc1764372514fac9e04e5d592fbd0878
Marcelo Tosco <marcelo@front.id>
2017-12-26 17:19:34 +0100

    - Separo el wizard a un archivo independiente y solo se usa si el usuario quiere pasar por el. - setup.sh ya es capaz de identificar la existencia de un archivo de configuración y aplicarlo si el usuario así lo quiere.

---
+++ make/questions-setup.sh
@@ -0,0 +1,109 @@
+# ---------
+
+while [[ -z "$domain" ]]
+do
+  read -p "¿Como llamamos a este proyecto? (Ejemplo frontid, agora, google, etc): " domain
+done
+
+export domain
+
+
+# ---------
+echo ''
+# ---------
+
+while [[ -z "$repo" ]]
+do
+  read -p "Indica la URL del repositorio (Ej:  git@github.com:githubtraining/hellogitworld.git): " repo
+done
+
+export repo
+
+# ---------
+echo ''
+# ---------
+
+while [[ -z "$db" ]]
+do
+  read -p "Puedes indicar el path de la DB si quieres que la carguemos inicialmente(Ej: $HOME/Downloads/bkp-$domain.sql). O pulsa intro si quires saltarte este paso: " db
+
+  # Exit loop if user just press enter.
+  if [[ $db = '' ]]; then
+    unset db
+    break
+  fi
+
+  # Check provided file exit and copy to the needed location.
+  if [ ! -f "$db" ]; then
+    echo "No encuentro la base de datos."
+    echo ""
+    unset db
+  fi
+done
+
+export db
+
+# ---------
+echo ''
+# ---------
+
+read -p "Si el repositorio tiene la aplicación web en un subdirectorio en lugar de en la raiz pon la ruta en el repositorio(Ej: web). O pulsa intro si la raiz del repositorio es la raiz de la aplicación web: " base_web_root
+
+export base_web_root
+
+# ---------
+echo ''
+# ---------
+
+PS3="Especifica la versión de PHP que vas a usar: "
+options=( '7.1-dev-3.3.1' '7.0-dev-3.3.1' '5.6-dev-3.3.1' '5.3-dev-3.3.1' )
+
+select phpver in "${options[@]}" ; do
+
+    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+        export phpver
+        break
+
+    else
+        echo "Se te fue el dedo, esa no es una opción válida."
+    fi
+done
+
+# ---------
+echo ''
+# ---------
+
+PS3="Y el servidor http: "
+options=( 'apache' 'nginx' 'both' )
+
+select webserver in "${options[@]}" ; do
+
+    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+        export webserver
+        break
+
+    else
+        echo "Se te fue el dedo, esa no es una opción válida."
+    fi
+done
+
+# ---------
+echo ''
+# ---------
+
+PS3="Y la versión de MariaBD: "
+options=( '10.1-x => compatible con MySQL 5.6' '10.2-x => Compatible con Mysql 5.7')
+
+select mysqlver in "${options[@]}" ; do
+
+    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+	mysqlver=$(echo $mysqlver| cut -d'-' -f 1)
+        export mysqlver
+        break
+
+    else
+        echo "Se te fue el dedo, esa no es una opción válida."
+    fi
+done
+
+echo $mysqlver
--- setup.sh
+++ setup.sh
@@ -17,115 +17,32 @@ echo -e "\e[32mPor favor ten a mano la url del repositorio del proyecto en el qu
 echo -e "\e[0m"
 echo ''
 
-# ---------
+# Si existe un archivo de configuración preguntamos si lo vamos a usar o si quiere pasar por el wizard.
+if [ ! -f "dockerizer-project.ini" ]; then
+  #Si no existe le hacemos le damos
+  source ./make/questions-setup.sh
+else
 
-while [[ -z "$domain" ]]
-do
-  read -p "¿Como llamamos a este proyecto? (Ejemplo frontid, agora, google, etc): " domain
-done
+  while ! { test "$run_from_conf_file" = 'y' || test "$run_from_conf_file" = 'n'; }; do
+    read -p "Actualmente existe un archivo de configuración con todos los parametros necesarios para echar a andar este proyecto. ¿Usamos el archivo de configuración (recomendado)? [Y/n]: " run_from_conf_file
 
-export domain
+    if [[ ${run_from_conf_file,,} = '' || ${run_from_conf_file,,} = 'y' ]]; then
+      run_from_conf_file='y'
+    elif [[ ${run_from_conf_file,,} = 'n' ]]; then
+      run_from_conf_file='n'
+    fi
+  done
 
-
-# ---------
-echo ''
-# ---------
-
-while [[ -z "$repo" ]]
-do
-  read -p "Indica la URL del repositorio (Ej:  git@github.com:githubtraining/hellogitworld.git): " repo
-done
-
-export repo
-
-# ---------
-echo ''
-# ---------
-
-while [[ -z "$db" ]]
-do
-  read -p "Puedes indicar el path de la DB si quieres que la carguemos inicialmente(Ej: $HOME/Downloads/bkp-$domain.sql). O pulsa intro si quires saltarte este paso: " db
-
-  # Exit loop if user just press enter.
-  if [[ $db = '' ]]; then
-    unset db
-    break
+  if [[ $run_from_conf_file = 'y' ]]; then
+    # Si ha decidido usar el archivo de configuración lo cargamos y seguimos.
+    source ./dockerizer-project.ini
+    else
+    # Si el usuario quiere omitir el archivo de configuración y pasar si o si por el wizard lo hacemos:
+    source ./make/questions-setup.sh
   fi
 
-  # Check provided file exit and copy to the needed location.
-  if [ ! -f "$db" ]; then
-    echo "No encuentro la base de datos."
-    echo ""
-    unset db
-  fi
-done
+fi
 
-export db
-
-# ---------
-echo ''
-# ---------
-
-read -p "Si el repositorio tiene la aplicación web en un subdirectorio en lugar de en la raiz pon la ruta en el repositorio(Ej: web). O pulsa intro si la raiz del repositorio es la raiz de la aplicación web: " base_web_root
-
-export base_web_root
-
-# ---------
-echo ''
-# ---------
-
-PS3="Especifica la versión de PHP que vas a usar: "
-options=( '7.1-dev-3.3.1' '7.0-dev-3.3.1' '5.6-dev-3.3.1' '5.3-dev-3.3.1' )
-
-select phpver in "${options[@]}" ; do 
-
-    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-        export phpver
-        break
-
-    else
-        echo "Se te fue el dedo, esa no es una opción válida."
-    fi
-done  
-
-# ---------
-echo ''
-# ---------
-
-PS3="Y el servidor http: "
-options=( 'apache' 'nginx' 'both' )
-
-select webserver in "${options[@]}" ; do 
-
-    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-        export webserver
-        break
-
-    else
-        echo "Se te fue el dedo, esa no es una opción válida."
-    fi
-done  
-
-# ---------
-echo ''
-# ---------
-
-PS3="Y la versión de MariaBD: "
-options=( '10.1-x => compatible con MySQL 5.6' '10.2-x => Compatible con Mysql 5.7')
-
-select mysqlver in "${options[@]}" ; do 
-
-    if (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
-	mysqlver=$(echo $mysqlver| cut -d'-' -f 1)
-        export mysqlver
-        break
-
-    else
-        echo "Se te fue el dedo, esa no es una opción válida."
-    fi
-done  
-
-echo $mysqlver
 
 echo ''
 echo -e "Esta el la configuración que nos queda:"
