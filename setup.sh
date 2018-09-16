115c059f95d49d383aedf196c36b644e21d15b85
Marcelo Tosco @capynet <marcelo@front.id>
2018-09-16 18:37:07 +0200

    Remove sudo as requirement for setup.sh since its not really required.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ $UID != 0 ]]; then
-    echo "Please run it as sudo:"
-    echo ''
-    echo -e "\e[32msudo -s $0 $*"
-    echo ''
-    exit 1
-fi
-
 # Include this folder into smartcd if is not included.
 if [ ! -f "$HOME/.smartcd/scripts$PWD" ]; then
   mkdir -p "$HOME/.smartcd/scripts$PWD"
@@ -18,7 +10,6 @@ fi
 source $HOME/.smartcd_config
 source $HOME/.smartcd/scripts$PWD/bash_enter
 
-# @todo remove when the perms get commited.
 chmod +x bin/ -R
 
 echo ''
