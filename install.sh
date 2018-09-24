bf1dbe0ef71f7a49b06b7c5db3ca0b1b73ab3840
Marcelo Tosco @capynet <marcelo@front.id>
2018-09-24 19:21:39 +0200

    Enhance a bit the install info.

--- install.sh
+++ install.sh
@@ -9,41 +9,36 @@ if [[ $UID != 0 ]]; then
 fi
 
 echo -e "Installing smartcd"
-echo ""
 
 # Install smartcd if not installed.
 if [ ! -f "$HOME/.smartcd_config" ]; then
   curl -L http://smartcd.org/install | bash
+else
+    echo -e "\e[32mCurrently installed\e[0m"
 fi
 
-
-echo -e "Creating a traefik docker network"
 echo ""
+echo -e "Creating a traefik docker network"
 
 network_exist="$(docker network ls | grep traefik_network)"
 
 if [ -z "$network_exist" ]; then
     docker network create traefik_network
+else
+    echo -e "\e[32mTraefik docker network currently installed\e[0m"
 fi
 
-echo -e "Installing traefik dockerizer proxy"
 echo ""
+echo -e "Installing/updating traefik dockerizer proxy"
 
-original_path=$PWD
-user_share_path="/usr/local/bin"
-traefik_path="$user_share_path/dk_traefik"
+traefik_path="/usr/local/bin/dk_traefik"
 mkdir -p $traefik_path
-
 cp setup_files/traefik-docker-compose.yml "$traefik_path/docker-compose.yml"
 cp -R setup_files/traefik $traefik_path
-cd $traefik_path
-docker-compose up -d
-cd $original_path
-echo ''
 
 # ---------
-echo -e "Installing dk cli"
 echo ""
+echo -e "Installing/updating dk cli"
 
 cp setup_files/dockerizer_cli /usr/local/bin/dk
 cp setup_files/dockerizer_cli_bash_autocomplete /etc/bash_completion.d/dk
@@ -51,4 +46,4 @@ chmod +x /usr/local/bin/dk
 
 # ---------
 echo ''
-echo -e "\e[32mEverything installed.!\e[0m"
+echo -e "\e[32m---- Everything installed! ----\e[0m"
